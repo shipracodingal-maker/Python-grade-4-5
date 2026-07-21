7836f23e8ad7b8ccc14eeb4c17fddf4683fe9cfc
shipracodingal-maker <shipra.codingal@gmail.com>
2026-07-21 13:05:55 +0800

    Add files via upload
    TypeCasting and datatype

---
+++ Typecasting and datatype.py
@@ -0,0 +1,43 @@
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print("Age :", age)
+print("Data Type of Age is", type(age))
+print("is_student :", is_student)
+print("Data Type of is_student is", type(is_student))
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
+
+
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name+last_name
+example = "Haa"*5
+
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("String Multiplied 5 times gives this result :", example)
+
+word = 'Coding'
+print("Length of String :", len(word))
+print("First Letter of String :", word[0])
+print("Last Letter of String :", word[5])
+print("String Sliced :", word[0:3])
+
+
+
+
+
